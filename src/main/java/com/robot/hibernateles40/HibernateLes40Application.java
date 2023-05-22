09546d39b59b7a3c4980503499496dccc1d630d2
”YevgenyDyachenko” <yevgeny.dyachenko@gmail.com>
2023-05-22 23:00:21 +0300

    Initial commit

---
+++ src/main/java/com/robot/hibernateles40/HibernateLes40Application.java
@@ -0,0 +1,13 @@
+package com.robot.hibernateles40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLes40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateLes40Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/robot/hibernateles40/HibernateLes40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.hibernateles40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateLes40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
